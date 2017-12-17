1e1430ca2a19926b7315964f8a1d5feb6ddf59c3
Stratadox <jesse@stratadox.com>
2017-12-17 13:27:57 +0100

    Add interface for defining how to sort

---
+++ contracts/DefinesHowToSort.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stratadox\Sorting;
+
+interface DefinesHowToSort
+{
+    public function field() : string;
+    public function next() : ?DefinesHowToSort;
+    public function ascends() : bool;
+}
