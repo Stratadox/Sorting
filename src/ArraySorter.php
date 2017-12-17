167548168994520293bbf81ab878157c50b1914c
Stratadox <jesse@stratadox.com>
2017-12-17 23:47:49 +0100

    Refactor ArraySorter class

--- src/ArraySorter.php
+++ src/ArraySorter.php
@@ -4,22 +4,38 @@ declare(strict_types=1);
 
 namespace Stratadox\Sorting;
 
+use Closure;
+
 class ArraySorter implements SortsTheElements
 {
     public function sortThe($elements, DefinesHowToSort $sorting) : array
     {
-        usort($elements, function (array $row1, array $row2) use ($sorting) {
-            while ($sorting->isRequired()) {
-                if ($row1[$sorting->field()] > $row2[$sorting->field()]) {
-                    return $sorting->ascends() ? 1 : -1;
-                }
-                if ($row1[$sorting->field()] < $row2[$sorting->field()]) {
-                    return $sorting->ascends() ? -1 : 1;
-                }
-                $sorting = $sorting->next();
-            }
-            return 0;
-        });
+        usort($elements, $this->function($sorting));
         return $elements;
+    }
+
+    private function function(DefinesHowToSort $sorting) : Closure
+    {
+        return function($row1, $row2) use ($sorting) {
+            return $this->doTheSorting($row1, $row2, $sorting);
+        };
+    }
+
+    private function doTheSorting(
+        $row1,
+        $row2,
+        DefinesHowToSort $sorting
+    ) : int
+    {
+        while ($sorting->isRequired()) {
+            if ($row1[$sorting->field()] > $row2[$sorting->field()]) {
+                return $sorting->ascends() ? 1 : -1;
+            }
+            if ($row1[$sorting->field()] < $row2[$sorting->field()]) {
+                return $sorting->ascends() ? -1 : 1;
+            }
+            $sorting = $sorting->next();
+        }
+        return 0;
     }
 }
