58b8f6aa9086bbf1eface00cc14862a6bbb58514
Stratadox <info@stratadox.com>
2018-04-12 23:24:08 +0200

    Improve multiple fields test case

--- tests/Unit/Sort_defines_how_to_sort.php
+++ tests/Unit/Sort_defines_how_to_sort.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Stratadox\Sorting\Test\Unit;
 
 use PHPUnit\Framework\TestCase;
-use Stratadox\Sorting\Contracts\DefinesHowToSort;
 use Stratadox\Sorting\Sort;
 
 /**
@@ -45,6 +44,7 @@ class Sort_defines_how_to_sort extends TestCase
     function sorting_can_involve_multiple_fields()
     {
         $sorting = Sort::descendingBy('foo', Sort::ascendingBy('bar'));
-        $this->assertInstanceOf(DefinesHowToSort::class, $sorting->next());
+        $this->assertSame('foo', $sorting->field());
+        $this->assertSame('bar', $sorting->next()->field());
     }
 }
