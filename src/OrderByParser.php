4224549f56ec91e3685ea7410e60b2e6948a02ad
Stratadox <info@stratadox.com>
2019-11-24 16:25:04 +0100

    Add basic parsing behaviour for OrderByParser

--- src/OrderByParser.php
+++ src/OrderByParser.php
@@ -2,12 +2,28 @@
 
 namespace Stratadox\Sorting;
 
+use function sprintf;
 use Stratadox\Sorting\Contracts\Sorting;
 
 class OrderByParser
 {
+    /** @var array */
+    private $sortableFields;
+
+    public function __construct(array $sortableFields = [])
+    {
+        $this->sortableFields = $sortableFields;
+    }
+
     public function parse(Sorting $sorting): string
     {
-        return '';
+        if (!$sorting->isRequired()) {
+            return '';
+        }
+        return sprintf(
+            'ORDER BY %s %s',
+            $this->sortableFields[$sorting->field()],
+            $sorting->ascends() ? 'ASC' : 'DESC'
+        );
     }
 }
--- tests/Unit/OrderByParser_builds_partial_sql_statements.php
+++ tests/Unit/OrderByParser_builds_partial_sql_statements.php
@@ -5,6 +5,7 @@ namespace Stratadox\Sorting\Test\Unit;
 use PHPUnit\Framework\TestCase;
 use Stratadox\Sorting\DoNotSort;
 use Stratadox\Sorting\OrderByParser;
+use Stratadox\Sorting\Sort;
 
 /**
  * @covers \Stratadox\Sorting\OrderByParser
@@ -16,5 +17,35 @@ class OrderByParser_builds_partial_sql_statements extends TestCase
     {
         $orderBy = new OrderByParser();
         $this->assertEquals('', $orderBy->parse(DoNotSort::atAll()));
+    }
+
+    /** @test */
+    function building_an_order_by_name_asc_statement()
+    {
+        $orderBy = new OrderByParser(['name' => 'name']);
+        $this->assertEquals(
+            'ORDER BY name ASC',
+            $orderBy->parse(Sort::ascendingBy('name'))
+        );
+    }
+
+    /** @test */
+    function building_an_order_by_name_desc_statement()
+    {
+        $orderBy = new OrderByParser(['name' => 'name']);
+        $this->assertEquals(
+            'ORDER BY name DESC',
+            $orderBy->parse(Sort::descendingBy('name'))
+        );
+    }
+
+    /** @test */
+    function building_an_order_by_table_dot_foo_asc_statement()
+    {
+        $orderBy = new OrderByParser(['foo' => 'table.foo']);
+        $this->assertEquals(
+            'ORDER BY table.foo ASC',
+            $orderBy->parse(Sort::ascendingBy('foo'))
+        );
     }
 }
